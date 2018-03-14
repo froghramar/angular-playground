757209e1a92c91ccbf9e55519aa35b5b8c23ef52
Feroz Ahmmed <froghramar@gmail.com>
2018-03-14 17:43:23 +0600

    add bengali localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+
+import localeFr from '@angular/common/locales/fr';
+import localeBn from '@angular/common/locales/bn';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeBn, 'bn');
 
 @NgModule({
 	declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
 	imports: [
 		BrowserModule
 	],
-	providers: [],
+	providers: [
+		{
+			provide: LOCALE_ID,
+			useValue: 'bn'
+		}
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
