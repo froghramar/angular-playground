0f5f4da434c04838631d4bd32fe67d15e3c78112
Feroz Ahmmed <froghramar@gmail.com>
2018-07-02 11:17:27 +0600

    add animation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatSnackBarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [
 		AppComponent
 	],
 	imports: [
-		BrowserModule
+		NoopAnimationsModule,
+		BrowserModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
