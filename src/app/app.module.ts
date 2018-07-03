f2ca2d49fa00e8c42bb390d25841985ba283946e
Feroz Ahmmed <froghramar@gmail.com>
2018-07-03 10:36:58 +0600

    add component product list and virtual scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MatSnackBarModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductListComponent } from './product-list/product-list.component';
+import { VirtualScrollModule } from './virtual-scroll/virtual-scroll.module';
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ProductListComponent
 	],
 	imports: [
 		NoopAnimationsModule,
 		BrowserModule,
+		VirtualScrollModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ src/app/virtual-scroll/virtual-scroll.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { VirtualScrollComponent } from './virtual-scroll.component';
+
+@NgModule({
+	imports: [
+		CommonModule,
+	],
+	declarations: [
+		VirtualScrollComponent
+	]
+})
+export class VirtualScrollModule { }
